e467775d32a4fa136774ecfb7896d491cd0e47db
Okunola Olubanjo <okunolaolubanjo@gmail.com>
2024-09-30 19:59:47 +0100

    Romoved some unnecessary

--- index.cjs
+++ index.cjs
@@ -10,8 +10,5 @@ fs.readFile(__filename, 'utf8', (err, data) => {
 
     const numberOfLines = lines.length;
 
-    let x;
-    let y;
-    let z;
     console.log(`The number of lines appears to be ${numberOfLines}`);
 })
